5ac1eeac96232d219fffb2b7f962a89f921ff8e5
jacokim406 <jacokim406@gmail.com>
2019-07-16 11:39:22 +0900

    initial commit

---
+++ jacokim/src/main/java/net/jacokim/JacokimApplication.java
@@ -0,0 +1,13 @@
+package net.jacokim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JacokimApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JacokimApplication.class, args);
+	}
+
+}
